c53a27fdbe69ec499599f7bc995fd589fc83f05e
李聪 <865000@nd.com>
2019-08-20 10:45:08 +0800

    Revert "引入ureport"
    This reverts commit 1032252ae8f9a52ac539413d4324ad6c5331b8ac.

--- src/main/java/com/licong/notemap/Application.java
+++ src/main/java/com/licong/notemap/Application.java
@@ -1,27 +1,17 @@
 package com.licong.notemap;
 
-import com.bstek.ureport.console.UReportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by lctm2005 on 2015/6/20.
  */
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
-@ImportResource("classpath:ureport-console-context.xml")
 public class Application extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public ServletRegistrationBean buildUReportServlet() {
-        return new ServletRegistrationBean(new UReportServlet(), "/ureport/*");
     }
 }
